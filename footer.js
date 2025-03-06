1d71047f3cd2290abf178a7ecdae9e23eeccdfe2
mayakomers <mayakomers@gmail.com>
2025-03-06 12:34:26 +0800

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-pink-500 text-white text-center py-4 mt-10">
+      <p>Hubungi Kami: 012-3781294 | Email: mayakomers@gmail.com</p>
+    </footer>
+  );
+}
