3ab3013f08fd49f68c777f785de879a81de01a7e
mayakomers <mayakomers@gmail.com>
2025-03-06 12:33:54 +0800

    Create header.js

---
+++ header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-pink-500 text-white text-center py-4 shadow-lg">
+      <h1 className="text-3xl font-bold">Maya Ecomm</h1>
+    </header>
+  );
+}
