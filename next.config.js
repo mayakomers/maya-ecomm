90f73d19eb863c2348b5ce146d98a606a6219028
mayakomers <mayakomers@gmail.com>
2025-03-06 12:58:48 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "public", // Tetapkan direktori output ke "public"
 };
+
+module.exports = nextConfig;
